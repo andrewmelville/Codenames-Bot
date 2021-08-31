49f61da4eacf36df9892eb5b996e489b689fee81
andrewmelville <andrewmelville.16@gmail.com>
2021-08-31 10:30:07 +0100

    player class

--- Exploration/player.py
+++ Exploration/player.py
@@ -2,6 +2,7 @@ class Player:
     
     def __init__(self):
         
+        # Take team colour as input
         self.my_team_colour = ''
         
         while self.my_team_colour not in ['Orange', 'Blue']:
@@ -13,16 +14,43 @@ class Player:
             else:
                 cand_colour = input("That's not a valid team colour. Please enter Blue or Orange.")
         
+        # Initialise scores and board words
         self.blue_score = 0
         self.orange_score = 0
         self.board_words = set([])
         
+        
+        # Take board words as input
         while len(self.board_words) < 25:
             
             cand_board_word = input("Which cards are on the board? {}/25".format(len(self.board_words)+1))
             self.board_words = self.board_words.union(set([cand_board_word]))
         
-        input("Who plays first, US/THEM?")
+        
+        # Choose role for bot
+        self.role = ''
+        
+        while self.role not in ['Spymaster', 'Operative']:
+            
+            cand_role = input("Am I the Spymaster or an Operative?")
+            
+            if cand_role in ['Spymaster', 'Operative']:
+                self.role = cand_role
+            else:
+                cand_role = input("Please enter either 'Spymaster' or 'Operative'.")
+        
+        
+        # Begin turn tracking
+        self.turn = ''
+        
+        while self.turn not in ['Us', 'Them']:
+            
+            cand_turn = input("Who plays first, Us/Them?")
+            
+            if cand_turn in ['Us', 'Them']:
+                self.turn = cand_turn
+            else:
+                cand_turn = input("Please enter either 'Us' or 'Them'.")
         
     def __call__(self):
         
