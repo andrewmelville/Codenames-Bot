409f93d89e80ca86563a7ed69d95f5ab2c246da8
Matt <matt.greenig@gmail.com>
2021-09-02 12:07:11 +0100

    reverted back to old version

--- utils/spymaster.py
+++ utils/spymaster.py
@@ -113,13 +113,8 @@ class SpyMaster(Game):
     # score a set of target words
     def score(self, targets: list) -> np.ndarray:
         
-        target_indices = targets if len(targets) > 1 else targets[0]
-        non_team_word_indices = self.word_indices['white'] + self.word_indices[self.other_team]
-        if len(non_team_word_indices) == 1:
-            non_team_word_indices = non_team_word_indices[0]
-        
-        target_similarities = self.proposal_board_similarities[:, target_indices]
-        non_team_word_similarities = self.proposal_board_similarities[:, non_team_word_indices]
+        target_similarities = self.proposal_board_similarities[:, targets]
+        non_team_word_similarities = self.proposal_board_similarities[:, self.word_indices['white'] + self.word_indices[self.other_team]]
         mean_target_similarities = target_similarities.mean(axis = 1)
         mean_non_team_word_similarities = non_team_word_similarities.mean(axis = 1)
         var_non_team_word_similarities = (non_team_word_similarities**2).mean(axis = 1)
